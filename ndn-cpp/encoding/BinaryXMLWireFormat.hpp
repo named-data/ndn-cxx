98848830e520c591685137107136aec15e20d336
Jeff Thompson <jefft0@gmail.com>
2013-06-28 12:50:00 -0700

    Comment out override methods until we implement them.

--- ndn-cpp/encoding/BinaryXMLWireFormat.hpp
+++ ndn-cpp/encoding/BinaryXMLWireFormat.hpp
@@ -14,11 +14,11 @@ namespace ndn {
 
 class BinaryXMLWireFormat : public WireFormat {
 public:
-  virtual void encodeName(Name &name, std::vector<unsigned char> &output);
+  //virtual void encodeName(Name &name, std::vector<unsigned char> &output);
   virtual void decodeName(Name &name, const unsigned char *input, unsigned int inputLength);
 
-  virtual void encodeInterest(Interest &interest, std::vector<unsigned char> &output);
-  virtual void decodeInterest(Interest &interest, const unsigned char *input, unsigned int inputLength);
+  //virtual void encodeInterest(Interest &interest, std::vector<unsigned char> &output);
+  //virtual void decodeInterest(Interest &interest, const unsigned char *input, unsigned int inputLength);
   
   static BinaryXMLWireFormat &instance() { return instance_; }
   
