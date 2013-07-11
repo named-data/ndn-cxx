4069ce9ba878117185481671c0691298c7dbcbcb
Jeff Thompson <jefft0@gmail.com>
2013-07-10 19:41:55 -0700

    Added encode and decode stubs.  Need to implement.

---
+++ ndn-cpp/c/encoding/BinaryXMLContentObject.c
@@ -0,0 +1,21 @@
+/**
+ * @author: Jeff Thompson
+ * Derived from ContentObject.js by Meki Cheraoui.
+ * See COPYING for copyright and distribution information.
+ */
+
+#include "BinaryXMLEncoder.h"
+#include "BinaryXMLDecoder.h"
+#include "BinaryXMLName.h"
+#include "BinaryXMLPublisherPublicKeyDigest.h"
+#include "BinaryXMLContentObject.h"
+
+ndn_Error ndn_encodeBinaryXMLContentObject(struct ndn_ContentObject *contentObject, struct ndn_BinaryXMLEncoder *encoder)
+{
+  return 0;
+}
+
+ndn_Error ndn_decodeBinaryXMLContentObject(struct ndn_ContentObject *contentObject, struct ndn_BinaryXMLDecoder *decoder)
+{
+  return 0;
+}
