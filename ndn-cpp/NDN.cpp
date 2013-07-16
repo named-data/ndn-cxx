707d70618dee2c1893641ec6d7e0f5c971c91199
Jeff Thompson <jefft0@gmail.com>
2013-07-16 16:32:40 -0700

    Added using namespace ndn::ptr_lib

--- ndn-cpp/NDN.cpp
+++ ndn-cpp/NDN.cpp
@@ -9,10 +9,11 @@
 #include "NDN.hpp"
 
 using namespace std;
+using namespace ndn::ptr_lib;
 
 namespace ndn {
 
-void NDN::expressInterest(const Name &name, const ptr_lib::shared_ptr<Closure> &closure, const Interest *interestTemplate)
+void NDN::expressInterest(const Name &name, const shared_ptr<Closure> &closure, const Interest *interestTemplate)
 {
   Interest interest(name);
   vector<unsigned char> encoding;
@@ -30,10 +31,10 @@ void NDN::onReceivedElement(unsigned char *element, unsigned int elementLength)
   BinaryXMLDecoder decoder(element, elementLength);
   
   if (decoder.peekDTag(ndn_BinaryXML_DTag_ContentObject)) {
-    ptr_lib::shared_ptr<ContentObject> contentObject(new ContentObject());
+    shared_ptr<ContentObject> contentObject(new ContentObject());
     contentObject->decode(element, elementLength);
     
-    ptr_lib::shared_ptr<Interest> dummyInterest;
+    shared_ptr<Interest> dummyInterest;
     UpcallInfo upcallInfo(this, dummyInterest, 0, contentObject);
     tempClosure_->upcall(UPCALL_CONTENT, upcallInfo);
   }
