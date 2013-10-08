32a23338d6eb8e12dbaabe930ca80f1f9cba0f69
Jeff Thompson <jefft0@gmail.com>
2013-10-07 18:39:00 -0700

    make: Use AC_TYPE_UINT8_T to check for uint8_t.

--- ndn-cpp/c/common.h
+++ ndn-cpp/c/common.h
@@ -8,36 +8,7 @@
 #define	NDN_COMMON_H
 
 #include "../../config.h"
-
-// Need to define uint8_t.  There may be no stdint.h because it is only part of the C standard since 1999.
-#if HAVE_STDINT
 #include <stdint.h>
-#else
-#endif
-#ifndef _UINT8_T
-#define _UINT8_T
-typedef unsigned char uint8_t;
-#endif
-
-#if HAVE_MEMCMP || HAVE_MEMCPY || HAVE_MEMSET
-// size_t is defined in memory.h, or something it includes.
-#include <memory.h>
-#else
-// Need to define size_t.
-#ifndef	_SIZE_T
-#define	_SIZE_T
-typedef	unsigned int size_t;
-#endif
-#endif
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-
-
-#ifdef	__cplusplus
-}
-#endif
+#include <stddef.h>
 
 #endif
