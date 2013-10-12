cf0fdd92f479dec970631514028c471b2d1fbe26
Jeff Thompson <jefft0@gmail.com>
2013-10-12 11:54:35 -0700

    security: Added IdentityCertificate stub.

---
+++ include/ndn-cpp/security/certificate/identity-certificate.hpp
@@ -0,0 +1,22 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2013 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * See COPYING for copyright and distribution information.
+ */
+
+#ifndef NDN_IDENTITY_CERTIFICATE_HPP
+#define	NDN_IDENTITY_CERTIFICATE_HPP
+
+#include "certificate.hpp"
+
+namespace ndn {
+
+class IdentityCertificate : public Certificate
+{
+
+};
+
+}
+
+#endif
