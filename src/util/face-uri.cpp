e3ef6eece27982a4fff8aa943332a21e193cb25b
Junxiao Shi <git@mail1.yoursunny.com>
2014-10-05 14:40:54 -0700

    util: Restore original copyright notice for the imported Ethernet and FaceUri files
    Change-Id: I752198181ad52bb57ffac5553d5d752373538241
    Refs: #1994

--- src/util/face-uri.cpp
+++ src/util/face-uri.cpp
@@ -1,6 +1,12 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2013-2014 Regents of the University of California.
+ * Copyright (c) 2014,  Regents of the University of California,
+ *                      Arizona Board of Regents,
+ *                      Colorado State University,
+ *                      University Pierre & Marie Curie, Sorbonne University,
+ *                      Washington University in St. Louis,
+ *                      Beijing Institute of Technology,
+ *                      The University of Memphis
  *
  * This file is part of ndn-cxx library (NDN C++ library with eXperimental eXtensions).
  *
@@ -23,6 +29,7 @@
 
 #include <boost/concept_check.hpp>
 #include <boost/regex.hpp>
+#include <boost/lexical_cast.hpp>
 
 namespace ndn {
 namespace util {
