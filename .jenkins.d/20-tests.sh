8b88160e94bfda66e8f63856f401f62cb174405f
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2014-11-20 19:58:07 -0800

    ci: Add ability to generate unit test reports in XML format (for xUnit processing)
    Change-Id: I67fdc579ed4562f7d9ac2c49f91892ddbb7c44c0

--- .jenkins.d/20-tests.sh
+++ .jenkins.d/20-tests.sh
@@ -16,4 +16,8 @@ fi
 ndnsec-keygen "/tmp/jenkins/$NODE_NAME" | ndnsec-install-cert -
 
 # Run unit tests
-./build/unit-tests -l test_suite
+if [[ -n "$XUNIT" ]]; then
+    ./build/unit-tests --log_format=XML --log_sink=build/xunit-report.xml --log_level=all --report_level=no
+else
+    ./build/unit-tests -l test_suite
+fi
