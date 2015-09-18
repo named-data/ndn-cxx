73e3004470ecf6ddb417927a653b96d9ee7ea7b2
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2015-09-17 17:09:51 -0700

    docs: Correct copyright statements
    Change-Id: Ie1a5a88a411e7d3893abeb5a810dff28ae3256b5

--- src/detail/registered-prefix.hpp
+++ src/detail/registered-prefix.hpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2013-2014 Regents of the University of California.
+ * Copyright (c) 2013-2015 Regents of the University of California.
  *
  * This file is part of ndn-cxx library (NDN C++ library with eXperimental eXtensions).
  *
