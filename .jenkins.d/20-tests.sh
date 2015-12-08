9fae3dbbc256e196f20b6a1730b3fc0030c452c1
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2015-12-07 18:04:32 -0800

    tests+ci: Enable dual XML and HRF output of unit test results
    Change-Id: I65e145e58cf94088bad0ca58f5ad4bc92d0b8c9d
    Refs: #2252

--- .jenkins.d/20-tests.sh
+++ .jenkins.d/20-tests.sh
@@ -17,7 +17,7 @@ ndnsec-keygen "/tmp/jenkins/$NODE_NAME" | ndnsec-install-cert -
 
 # Run unit tests
 if [[ -n "$XUNIT" ]]; then
-    ./build/unit-tests --log_format=XML --log_sink=build/xunit-report.xml --log_level=all --report_level=no
+    ./build/unit-tests --log_format2=XML --log_sink2=build/xunit-report.xml --log_level=all
 else
     ./build/unit-tests -l test_suite
 fi
