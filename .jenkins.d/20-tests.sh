1b6187ff70422ad53c4d6ee39e4e71bbee255974
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2016-01-18 22:41:49 +0100

    ci: Fix unit test execution with boost 1.60
    Change-Id: I8bdefd6d5e082f01c827fbe4a92ce5c7952b8bd1
    Refs: #3417

--- .jenkins.d/20-tests.sh
+++ .jenkins.d/20-tests.sh
@@ -17,7 +17,7 @@ ndnsec-keygen "/tmp/jenkins/$NODE_NAME" | ndnsec-install-cert -
 
 # Run unit tests
 if [[ -n "$XUNIT" ]]; then
-    ./build/unit-tests --log_format2=XML --log_sink2=build/xunit-report.xml --log_level=all
+    ./build/unit-tests --log_level=all -- --log_format2=XML --log_sink2=build/xunit-report.xml
 else
     ./build/unit-tests -l test_suite
 fi
