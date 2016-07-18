09bcab5bb80f655fd75271440c6cce03147fcb45
Junxiao Shi <git@mail1.yoursunny.com>
2016-07-18 02:43:34 +0000

    lp: fix typo
    refs #2879
    
    Change-Id: Iaa5aeb4caaecd835ae840038d703251ff950e66d

--- src/lp/detail/field-decl.hpp
+++ src/lp/detail/field-decl.hpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2013-2015 Regents of the University of California.
+ * Copyright (c) 2013-2016 Regents of the University of California.
  *
  * This file is part of ndn-cxx library (NDN C++ library with eXperimental eXtensions).
  *
@@ -142,6 +142,6 @@ public:
 
 } // namespace detail
 } // namespace lp
-} // namesapce ndn
+} // namespace ndn
 
 #endif // NDN_CXX_LP_DETAIL_FIELD_DECL_HPP
