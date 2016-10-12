69ec4549d8f7c3ebfd6934d0594565e402dd8d0f
Davide Pesavento <davide.pesavento@lip6.fr>
2016-10-12 20:03:09 +0200

    ci: ensure XML test reports do not clobber the output of previous runs
    Change-Id: I3be7022f3171fc56837bdd5c51c8684a5dda0ac4
    Refs: #3750

--- .jenkins.d/20-tests.sh
+++ .jenkins.d/20-tests.sh
@@ -18,13 +18,12 @@ ndnsec-keygen "/tmp/jenkins/$NODE_NAME" | ndnsec-install-cert -
 BOOST_VERSION=$(python -c "import sys; sys.path.append('build/c4che'); import _cache; print(_cache.BOOST_VERSION_NUMBER);")
 
 ut_log_args() {
-    ((ut_count+=1))
     if (( BOOST_VERSION >= 106200 )); then
-        echo --logger=HRF,test_suite,stdout:XML,all,build/xunit-report-${ut_count}.xml
+        echo --logger=HRF,test_suite,stdout:XML,all,build/xunit-${1:-report}.xml
     else
         if [[ -n $XUNIT ]]; then
             echo --log_level=all $( (( BOOST_VERSION >= 106000 )) && echo -- ) \
-                 --log_format2=XML --log_sink2=build/xunit-report-${ut_count}.xml
+                 --log_format2=XML --log_sink2=build/xunit-${1:-report}.xml
         else
             echo --log_level=test_suite
         fi
@@ -44,5 +43,3 @@ export ASAN_OPTIONS
 
 # Run unit tests
 ./build/unit-tests $(ut_log_args)
-
-unset ut_count
