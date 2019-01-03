bfe3139edeaab500f7890ba7d3dc4439f91f0b3c
Davide Pesavento <davide.pesavento@lip6.fr>
2019-01-03 16:26:51 -0500

    util: seed the PRNG with more entropy
    Change-Id: I0e54079bac039db0a10b2f443a3b02b85dd2bb68

--- ndn-cxx/util/random.cpp
+++ ndn-cxx/util/random.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2013-2018 Regents of the University of California.
+ * Copyright (c) 2013-2019 Regents of the University of California.
  *
  * This file is part of ndn-cxx library (NDN C++ library with eXperimental eXtensions).
  *
@@ -55,8 +55,14 @@ generateSecureBytes(uint8_t* bytes, size_t size)
 static std::mt19937&
 getRandomGenerator()
 {
-  static std::mt19937 rng{std::random_device{}()};
-  return rng;
+  static optional<std::mt19937> rng;
+  if (!rng) {
+    std::random_device rd;
+    // seed with 256 bits of entropy
+    std::seed_seq seeds{rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
+    rng.emplace(seeds);
+  }
+  return *rng;
 }
 
 uint32_t
