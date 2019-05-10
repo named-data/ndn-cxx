3510d299ba6823c5342c5d565d190ce14f427ad8
Davide Pesavento <davidepesa@gmail.com>
2019-05-10 14:57:33 -0400

    lp: add missing std::move to Nack constructor
    Change-Id: I15a304caea4df2de1b804554e2cd60e74bd93c0d

--- ndn-cxx/lp/nack.cpp
+++ ndn-cxx/lp/nack.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2013-2018 Regents of the University of California.
+ * Copyright (c) 2013-2019 Regents of the University of California.
  *
  * This file is part of ndn-cxx library (NDN C++ library with eXperimental eXtensions).
  *
@@ -34,7 +34,7 @@ Nack::Nack(const Interest& interest)
 }
 
 Nack::Nack(Interest&& interest)
-  : m_interest(interest)
+  : m_interest(std::move(interest))
 {
 }
 
