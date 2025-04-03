1c92524cbcd9f9023e86297f18ff09f677baec78
Davide Pesavento <davidepesa@gmail.com>
2025-04-03 14:32:36 -0400

    ci: upgrade gcovr to the latest version
    And adjust lcov invocation for lcov 2.1
    
    Change-Id: I03156b21f2675e105a2a994d2b5f41ceecc490ec

--- .jenkins.d/30-coverage.sh
+++ .jenkins.d/30-coverage.sh
@@ -8,31 +8,38 @@ export UV_NO_MANAGED_PYTHON=1
 
 set -x
 
-# Generate an XML report (Cobertura format) and a detailed HTML report using gcovr
+# Generate a detailed HTML report and an XML report in Cobertura format using gcovr
 # Note: trailing slashes are important in the paths below. Do not remove them!
-uvx gcovr@5.2 \
-    --object-directory build \
+uvx --from 'git+https://github.com/gcovr/gcovr@99b82e7' gcovr \
+    --decisions \
     --filter ndn-cxx/ \
     --exclude ndn-cxx/detail/nonstd/ \
     --exclude-throw-branches \
     --exclude-unreachable-branches \
     --cobertura build/coverage.xml \
     --html-details build/gcovr/ \
-    --print-summary
+    --txt-summary \
+    build
 
 # Generate a detailed HTML report using lcov
-lcov --quiet \
-     --capture \
-     --directory . \
-     --exclude "$PWD/ndn-cxx/detail/nonstd/*" \
-     --exclude "$PWD/tests/*" \
-     --no-external \
-     --rc lcov_branch_coverage=1 \
-     --output-file build/coverage.info
+lcov \
+    --quiet \
+    --capture \
+    --directory . \
+    --include "$PWD/ndn-cxx/*" \
+    --exclude "$PWD/ndn-cxx/detail/nonstd/*" \
+    --ignore-errors count,inconsistent \
+    --branch-coverage \
+    --rc no_exception_branch=1 \
+    --output-file build/coverage.info
 
-genhtml --branch-coverage \
-        --demangle-cpp \
-        --legend \
-        --output-directory build/lcov \
-        --title "ndn-cxx unit tests" \
-        build/coverage.info
+genhtml \
+    --quiet \
+    --branch-coverage \
+    --demangle-cpp \
+    --legend \
+    --missed \
+    --show-proportion \
+    --title "ndn-cxx $(cat VERSION.info)" \
+    --output-directory build/lcov \
+    build/coverage.info
